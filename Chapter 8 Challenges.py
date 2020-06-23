8c5e845e0f2d06d7513802c877bdf927fbf6297d
Zach Siegel <zachsiegel@vals-mbp.lan>
2020-06-23 16:26:13 -0500

    challenges from chapter 8

---
+++ Chapter 8 Challenges.py
@@ -0,0 +1,11 @@
+import math
+import random
+import statistics
+import keyword
+import cubed
+
+print(random.randint(1, 100))
+nums = [1, 4, 74, 1234, 130948]
+print(statistics.median(nums))
+print(keyword.iskeyword("in"))
+print(cubed.cube())
